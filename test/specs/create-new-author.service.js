5ea39987f5c8f58161d165873a68e6846a6ca3bf
MJG <MJG@MacBook-Pro.local>
2016-05-04 14:56:58 -0400

    Setting up testing

---
+++ test/specs/create-new-author.service.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict';
+
+  var assert = chai.assert;
+
+  suite('create new author service', function () {
+
+    setup(module('blog'));
+
+    setup(inject(function () {
+
+    }))
+  });
+})();
